e5d7de8a6696f7e5ccd0a94a8ee2fb69e122e415
tiba1111 <tiba.alramadan@snhu.edu>
2025-06-15 19:57:48 +0400

    module6 completed baseline

--- app_api/controllers/trips.js
+++ app_api/controllers/trips.js
@@ -37,8 +37,57 @@ const tripsFindByCode = async (req, res) => {
     return res.status(200).json(q);
   }
 };
+const tripsAddTrip = async (req, res) => {
+    const newTrip = new Trip({
+        code: req.body.code,
+        name: req.body.name,
+        length: req.body.length,
+        start: req.body.start,
+        resort: req.body.resort,
+        perPerson: req.body.perPerson,
+        image: req.body.image,
+        description: req.body.description,
+    });
+  const q = await newTrip.save();
+
+    // database returned no data
+    if (!q) {
+        return res.status(400).json(err);
+    }
+    // Return new trip
+    else {
+        return res.status(201).json(q);
+    }
+};
+// PUT methodAdd commentMore actions
+const tripsUpdateTrip = async (req, res) => {
+    const q = await Model.findOneAndUpdate(
+        { code: req.params.tripCode },
+        {
+            code: req.body.code,
+            name: req.body.name,
+            length: req.body.length,
+            start: req.body.start,
+            resort: req.body.resort,
+            perPerson: req.body.perPerson,
+            image: req.body.image,
+            description: req.body.description,
+        }
+    ).exec();
+
+    // database returned no data
+    if (!q) {
+        return res.status(400).json(err);
+    }
+    // Return new trip
+    else {
+        return res.status(201).json(q);
+    }
+};
 
 module.exports = {
   tripsList,
   tripsFindByCode,
+  tripsAddTrip,
+  tripsUpdateTrip,
 };

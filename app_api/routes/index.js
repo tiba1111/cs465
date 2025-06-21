195c4aa4f3e250744f5f1e2915f84c7ce3644074
tiba1111 <tiba.alramadan@snhu.edu>
2025-06-21 13:05:24 +0400

    Revert "module6 completed baseline" n
    This reverts commit e5d7de8a6696f7e5ccd0a94a8ee2fb69e122e415.

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -5,16 +5,9 @@ const router = express.Router(); // Router logic
 const tripsController = require("../controllers/trips");
 
 // define route for our trips endpoint
-router
-    .route("/trips")
-    .get(tripsController.tripsList)          // GET method routes tripsList
-    .post(tripsController.tripsAddTrip);     // POST method adds a Trip
+router.route("/trips").get(tripsController.tripsList);
+
 // GET method routes tripsByCode - requires parameter
-router
-    .route("/trips/:tripCode")
-    .get(tripsController.tripsFindByCode)
-
-    .put(tripsController.tripsUpdateTrip);
-
+router.route("/trips/:tripCode").get(tripsController.tripsFindByCode);
 
 module.exports = router;
